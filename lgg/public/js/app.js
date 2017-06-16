ec545fd3935ac77362e23a1affab3276663e2388
Boyang <wangboyang1991@gmail.com>
2017-06-16 20:37:57 +0800

    done front and back dummy impl
    To add pre-generated text and use in sequence

--- lgg/public/js/app.js
+++ lgg/public/js/app.js
@@ -1,1 +1,10 @@
 $('.menu .item').tab();
+
+$('.generate-button').click(function(e) {
+    $.ajax('/getGeneratedText', {
+        success: function(data) {
+            console.log(data);
+            $('.generated-text').text(data.text);
+        }
+    });
+});
