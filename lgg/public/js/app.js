3c44f8fc1030ed7f7f5d08a8dcbc55502b1b0696
Boyang <wangboyang1991@gmail.com>
2017-07-24 20:05:07 +0800

    change ajax url back to no /lgg

--- lgg/public/js/app.js
+++ lgg/public/js/app.js
@@ -1,7 +1,7 @@
 $('.menu .item').tab();
 
 $('.generate-button').click(function(e) {
-    $.ajax('/lgg/getGeneratedText', {
+    $.ajax('./getGeneratedText', {
         success: function(data) {
             console.log(data);
             $('.generated-text').text(data.text);
